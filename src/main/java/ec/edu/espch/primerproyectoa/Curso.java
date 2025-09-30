45b5d1a9ebe9898fab05bed673a050a6b97c808a
AliciaLliguin <alicialliguin37@gmail.com>
2025-09-30 12:06:05 -0500

    Creación de la clases estudiante y curso

---
+++ src/main/java/ec/edu/espch/primerproyectoa/Curso.java
@@ -0,0 +1,24 @@
+
+package ec.edu.espch.primerproyectoa;
+
+
+public class Curso {
+    //Atributos
+    public String nombre;
+    public int codigo;
+    public String lista;
+    //Metodos
+     //Metodos
+    public void agregarEstudiante()
+    {
+        
+    }
+    public void eliminarEstudiante()
+    {
+        
+    }
+    public void listaEstudiante()
+    {
+        
+    }
+}
---
+++ src/main/java/ec/edu/espch/primerproyectoa/Estudiante.java
@@ -0,0 +1,31 @@
+
+package ec.edu.espch.primerproyectoa;
+
+
+public class Estudiante{
+    //Atributos
+    public String nombre;
+    public String identificacion;
+    public int calificacionPromedio;
+    //Metodos
+    public void inscribirse()
+    {
+        
+    }
+    public void retirarse()
+    {
+        
+    }
+    public void mostrarNombre()
+    {
+        
+    }
+    public void mostrarIdentificacion()
+    {
+        
+    }
+    public void mostrarPromedio()
+    {
+        
+    }
+}
